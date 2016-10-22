c5ddbb54414af77eb3595e3069433c38d9dd548a
Albert <al.zoraw@gmail.com>
2016-10-22 19:01:58 +0200

    Client Frame

---
+++ Client/src/ClientFrame.java
@@ -0,0 +1,5 @@
+public class ClientFrame {
+    public ClientFrame(String clientFrame) {
+
+    }
+}
--- Client/src/SuperFileBackupperClient.java
+++ Client/src/SuperFileBackupperClient.java
@@ -1,5 +1,12 @@
+import java.awt.*;
+
 public class SuperFileBackupperClient {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new ClientFrame("ClientFrame");
+            }
+        });
     }
 }
